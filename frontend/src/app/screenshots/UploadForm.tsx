c82b777171fe9264a43b2d1c9ccb91222893cb97
David Lema <lemadavid1234@gmail.com>
2026-07-23 21:04:16 -0400

    feat(frontend): add upload feedback to screenshot form

--- frontend/src/app/screenshots/UploadForm.tsx
+++ frontend/src/app/screenshots/UploadForm.tsx
@@ -1,36 +1,62 @@
 "use client"; //directive marking this file's boundary into client rendered code
 
 import { useState } from "react"
+import { useRouter } from "next/navigation" //must be used inside a Client-Component
 
 export default function UploadForm() {
 
     const [file, setFile] = useState<File | null>(null);
+    const [status, setStatus] = useState<"idle" | "uploading" | "success" | "error">("idle");
+    const router = useRouter();
 
     async function handleSubmit(e: React.SubmitEvent<HTMLFormElement>) {
-        e.preventDefault();
+        e.preventDefault(); //prevent full page reload/navifgation so the fetch below can run instead
         if (!file) return; //if no file, return
+
+        setStatus("uploading");
 
         const formData = new FormData();
         formData.append("file", file);
 
-        //POST endpoint
-        await fetch("http://localhost:8000/screenshots", {
-            method: "POST",
-            body: formData,
-        });
+        try {
+            //POST endpoint
+            const res = await fetch("http://localhost:8000/screenshots", {
+                method: "POST",
+                body: formData,
+            });
+
+            if (!res.ok) {
+                setStatus("error");
+                return;
+            }
+
+            //successful POST
+            setStatus("success");
+            router.refresh();
+
+        } catch (error) {
+            //only fires for actual network failures (server unreachable, not HTTP codes)
+            setStatus("error")
+            console.log(`Unsuccessful Upload: ${error}`)
+        }
     }
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input
-                type="file"
-                onChange={(e) => setFile(e.target.files?.[0] ?? null)}
-            />
-            <button type="submit">Upload</button>
-        </form>
+        <>
+            <form onSubmit={handleSubmit}>
+                <input
+                    type="file"
+                    onChange={(e) => setFile(e.target.files?.[0] ?? null)}
+                />
+
+                <button type="submit" disabled={status === "uploading"}>
+                    {status === "uploading" ? "uploading..." : "Upload"}
+                </button>
+            </form>
+            {status === "success" && <p>Successful Upload.</p>}
+            {status === "error" && <p>Failed to Upload. Try again.</p>}
+        </>
     )
-
-
 
 }
 
